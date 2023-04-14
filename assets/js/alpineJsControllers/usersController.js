4300ee871960479d531308920d96bfe24d60e7c7
MohHussainIshraq <mhussain_ishraq@yahoo.com>
2023-04-14 18:47:08 +0430

    x-if & x-show

--- assets/js/alpineJsControllers/usersController.js
+++ assets/js/alpineJsControllers/usersController.js
@@ -2,12 +2,14 @@ document.addEventListener('alpine:init', () => {
     Alpine.data('usersData', function(){
         return {
             users: [],
+            isLoading: false,
             getUsers(){
+                this.isLoading = true
                 axios.get("https://jsonplaceholder.typicode.com/users").then((res)=>{
                     this.users = res.data
-                    console.log(res);
-                })
+                    this.isLoading = false;
+                });
             }            
         }
-    })
-})
+    });
+});
