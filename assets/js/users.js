c2188b3b154bb62bf8408bc718cec351cab35fd3
MohHussainIshraq <mhussain_ishraq@yahoo.com>
2023-04-16 05:17:21 +0430

    alpinejs-x-bind

--- assets/js/users.js
+++ assets/js/users.js
@@ -2,18 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, {});
 
-    const addUserButton = document.getElementById('add_user_button');
-    addUserButton.addEventListener('click', ()=>{
-        document.querySelector('.add_user_model').classList.add('show');
-        document.querySelector('.add_user_model_back').classList.remove('dis-none');
+    // const addUserButton = document.getElementById('add_user_button');
+    // addUserButton.addEventListener('click', ()=>{
+    //     document.querySelector('.add_user_model').classList.add('show');
+    //     document.querySelector('.add_user_model_back').classList.remove('dis-none');
 
-    });
+    // });
 
-    document.querySelector('.add_user_model_back').addEventListener('click', (e)=>{
-        document.querySelector('.add_user_model').classList.remove('show');
-        e.target.classList.add('dis-none');
+    // document.querySelector('.add_user_model_back').addEventListener('click', (e)=>{
+    //     document.querySelector('.add_user_model').classList.remove('show');
+    //     e.target.classList.add('dis-none');
 
-    });
-
+    // });
 
 });
