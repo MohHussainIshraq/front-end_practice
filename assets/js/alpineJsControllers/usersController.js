257e3682184bb22013d91ffacb9ce209fe33e2cb
MohHussainIshraq <mhussain_ishraq@yahoo.com>
2023-04-17 06:16:39 +0430

    pagination part one

--- assets/js/alpineJsControllers/usersController.js
+++ assets/js/alpineJsControllers/usersController.js
@@ -1,15 +1,30 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('usersData',()=>({
             users: [],
+            pageUsers: [],
             isLoading: false,
             showAddModal: false,
+            pageCount: 1,
+            itemsCount: 4,
+            currentPage: 3,
             getUsers(){
                 this.isLoading = true
                 axios.get("https://jsonplaceholder.typicode.com/users").then((res)=>{
                     this.users = res.data
+                    this.pagination()
                 }).finally(()=>{
                 this.isLoading = false
                 })
-            }            
+            },
+            pagination(){
+                this.pageCount = Math.ceil(this.users.length / this.itemsCount) // 10 / 4 = 3
+                let start = (this.currentPage * this.itemsCount) - this.itemsCount
+                let end = this.currentPage * this.currentPage * this.itemsCount
+                this.pageUsers = this.users.slice(start, end)
+                console.log(this.pageUsers)
+
+
+                // this.users.slice(0,3)
+            }        
     }))
 })
